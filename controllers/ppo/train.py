ab8a41c54472d0c643a470bc4f3e795ac92a7260
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2024-05-30 16:17:19 +0200

    added evaluation environment

--- controllers/ppo/train.py
+++ controllers/ppo/train.py
@@ -12,13 +12,12 @@ import yaml
 from munch import munchify
 from safe_control_gym.utils.registration import make
 from stable_baselines3 import PPO
-from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.callbacks import EvalCallback
 
 from lsy_drone_racing.constants import FIRMWARE_FREQ
 from lsy_drone_racing.wrapper import DroneRacingWrapper
 
 logger = logging.getLogger(__name__)
-
 
 
 def create_race_env(config_path: Path, gui: bool = False) -> DroneRacingWrapper:
@@ -44,17 +43,37 @@ def main(config: str = "../config/train0.yaml", gui: bool = False, log_level: in
     """Create the environment, check its compatibility with sb3, and run a PPO agent."""
     logging.basicConfig(level=log_level)
     config_path = Path(__file__).resolve().parents[1] / config
-    env = create_race_env(config_path=config_path, gui=gui)
+
+    env = create_race_env(config_path=config_path, gui=False)
+    eval_env = create_race_env(config_path=config_path, gui=gui)
+
+    eval_callback = EvalCallback(
+        eval_env,
+        best_model_save_path="./models/",
+        log_path="./logs/",
+        eval_freq=100_000,
+        deterministic=True,
+    )
+
     # Sanity check to ensure the environment conforms to the sb3 API
-    check_env(env)
-    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="logs", learning_rate=0.02)
+    # check_env(env)
+    model = PPO(
+        "MlpPolicy",
+        env,
+        learning_rate=6e-4,
+        verbose=1,
+        tensorboard_log="logs",
+    )  # Train the agent
 
-    # Train the agent
     train_name = f"ppo_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
-    model.learn(total_timesteps=100_000, progress_bar=True,
-                log_interval=1, tb_log_name=train_name)
+    model.learn(
+        total_timesteps=100_000,
+        progress_bar=True,
+        log_interval=1,
+        tb_log_name=train_name,
+        callback=eval_callback,
+    )
     model.save(f"models/{train_name}")
-
 
 if __name__ == "__main__":
     fire.Fire(main)
