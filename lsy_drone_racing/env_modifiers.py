3817d2c20dd6ca56877599edca612f3e1339c939
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2024-05-30 16:17:55 +0200

    new limits for space (solving problem of flying to ceiling) + action transform

--- lsy_drone_racing/env_modifiers.py
+++ lsy_drone_racing/env_modifiers.py
@@ -16,7 +16,7 @@ class ObservationParser:
         self,
         n_gates: int,
         n_obstacles: int,
-        drone_pos_limits: list = [5, 5, 5],
+        drone_pos_limits: list = [3, 3, 2],
         drone_yaw_limits: list = [np.pi],
         gate_pos_limits: list = [5, 5, 5],
         gate_yaw_limits: list = [np.pi],
@@ -55,9 +55,13 @@ class ObservationParser:
         # Hidden states that are not part of the observation space
         self.just_passed_gate: bool = False
 
+    def uninitialized(self) -> bool:
+        """Check if the observation parser is uninitialized."""
+        return self.drone_pos is None
+
     def out_of_bounds(self) -> bool:
         """Check if the drone is out of bounds."""
-        return self.observation_space.contains(self.get_observation())
+        return not self.observation_space.contains(self.get_observation())
 
     def update(self, obs: np.ndarray, info: dict[str, Any]):
         """Update the observation parser with the new observation and info dict.
@@ -97,7 +101,7 @@ class ObservationParser:
                 [self.gate_id],
             ]
         )
-        return obs
+        return obs.astype(np.float32)
 
 
 class Rewarder:
@@ -106,14 +110,19 @@ class Rewarder:
     def __init__(
         self,
         collision: float = -1000.0,
+        out_of_bounds: float = -3000.0,
+        times_up: float = -1000.0,
         end_reached: float = 1000.0,
+        close_to_gate: float = 0.0,
         dist_to_gate_mul: float = -1.0,
         dist_to_obstacle_mul: float = 0.2,
-        gate_reached: float = 100.0,
+        gate_reached: float = 1000.0,
     ):
         """Initialize the rewarder."""
         self.collision = collision
+        self.out_of_bounds = out_of_bounds
         self.end_reached = end_reached
+        self.close_to_gate = close_to_gate
         self.dist_to_gate_mul = dist_to_gate_mul
         self.dist_to_obstacle_mul = dist_to_obstacle_mul
         self.gate_reached = gate_reached
@@ -147,14 +156,18 @@ class Rewarder:
         """
         reward = 0.0
 
-        if info["collision"][1] or obs.out_of_bounds():
+        if info["collision"][1]:
             return self.collision
+
+        if obs.out_of_bounds():
+            return self.out_of_bounds
+
         if info["task_completed"]:
             return self.end_reached
 
         # Reward for gating close to the next gate
         dist_to_gate = np.linalg.norm(obs.drone_pos - obs.gates_pos[obs.gate_id])
-        reward += dist_to_gate * self.dist_to_gate_mul
+        reward += self.close_to_gate + dist_to_gate * self.dist_to_gate_mul
 
         # Reward for avoiding obstacles
         dist_to_obstacle = np.linalg.norm(obs.drone_pos - obs.obstacles_pos[obs.obstacles_in_range])
@@ -181,3 +194,36 @@ def map_reward_to_color(reward: float) -> str:
     if reward < 0:
         return "red"
     return "green"
+
+
+def transform_action(
+    raw_action: np.ndarray,
+    observation_parser: ObservationParser,
+    transform_type: str = "relative",
+    pos_scaling: np.array = [1.0, 1.0, 1.0],
+    yaw_scaling: float = np.pi,
+) -> np.ndarray:
+    """Transform the raw action to the action space.
+
+    Args:
+        raw_action: The raw action from the model is in the range [-1, 1].
+        observation_parser: The observation parser.
+        transform_type: The type of transformation, either "relative" or "absolute".
+        pos_scaling: The scaling of the position.
+        yaw_scaling: The scaling of the angle
+
+    Returns:
+        The transformed action to control the drone.
+    """
+    if transform_type == "relative":
+        action_transform = np.zeros(14)
+        action_transform[:3] = observation_parser.drone_pos + raw_action[:3]
+        action_transform[9] = yaw_scaling * raw_action[3]
+
+    elif transform_type == "absolute":
+        action_transform = np.zeros(14)
+        scaled_action = raw_action * np.concatenate([pos_scaling, [yaw_scaling]])
+        action_transform[:3] = scaled_action[:3]
+        action_transform[9] = scaled_action[3]
+
+    return action_transform
