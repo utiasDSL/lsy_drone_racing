bad0e0a6cc6c02aebbdb4211602bcb55f017c1c8
Martin Schuck <martin.schuck@tum.de>
2024-10-10 17:52:09 +0200

    Change default controller

--- scripts/sim.py
+++ scripts/sim.py
@@ -31,7 +31,7 @@ logger = logging.getLogger(__name__)
 
 def simulate(
     config: str = "config/level0.toml",
-    controller: str = "examples/controller.py",
+    controller: str = "examples/trajectory_controller.py",
     n_runs: int = 1,
     gui: bool = True,
 ) -> list[float]:
