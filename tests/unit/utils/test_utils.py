95f58cea25d28ed04431849839a73a958cf22eda
Martin Schuck <martin.schuck@tum.de>
2025-05-07 16:39:27 +0200

    Fix linting

--- tests/unit/utils/test_utils.py
+++ tests/unit/utils/test_utils.py
@@ -1,15 +1,13 @@
 from pathlib import Path
 
-import gymnasium
 import numpy as np
 import pytest
-from gymnasium.wrappers.jax_to_numpy import JaxToNumpy
 from ml_collections import ConfigDict
 from scipy.spatial.transform import Rotation as R
 
 from lsy_drone_racing.control.controller import Controller
 from lsy_drone_racing.envs.utils import gate_passed
-from lsy_drone_racing.utils import draw_line, load_config, load_controller
+from lsy_drone_racing.utils import load_config, load_controller
 
 
 @pytest.mark.unit
