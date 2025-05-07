ccbb80288a90fbef3f7dada9806fe15f0462e950
Martin Schuck <martin.schuck@tum.de>
2025-05-07 10:27:26 +0200

    Change sensor range to 0.7. Add draw_line util

--- tests/unit/utils/test_utils.py
+++ tests/unit/utils/test_utils.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 
+import gymnasium
 import numpy as np
 import pytest
+from gymnasium.wrappers.jax_to_numpy import JaxToNumpy
 from ml_collections import ConfigDict
 from scipy.spatial.transform import Rotation as R
 
 from lsy_drone_racing.control.controller import Controller
 from lsy_drone_racing.envs.utils import gate_passed
-from lsy_drone_racing.utils import load_config, load_controller
+from lsy_drone_racing.utils import draw_line, load_config, load_controller
 
 
 @pytest.mark.unit
@@ -52,3 +54,25 @@ def test_gate_pass():
     # Test not crossing the plane
     drone_pos, last_drone_pos = np.array([0, -0.5, 0]), np.array([0, -1, 0])
     assert not gate_passed(drone_pos, last_drone_pos, gate_pos, gate_quat, gate_size)
+
+
+@pytest.mark.unit
+def test_draw_line():
+    config = load_config(Path(__file__).parents[3] / "config/level0.toml")
+    env = gymnasium.make(
+        "DroneRacing-v0",
+        freq=config.env.freq,
+        sim_config=config.sim,
+        sensor_range=config.env.sensor_range,
+        track=config.env.track,
+        disturbances=config.env.get("disturbances"),
+        randomizations=config.env.get("randomizations"),
+        seed=config.env.seed,
+    )
+    env = JaxToNumpy(env)
+    env.reset()
+    for _ in range(3):
+        line = np.stack([np.arange(4), np.zeros(4), np.zeros(4)]).T
+        draw_line(env, line)
+        env.render()
+    env.close()
