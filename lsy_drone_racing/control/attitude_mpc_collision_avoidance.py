9fe9225e0e78bcaaf97dc911a60985fadcfec17a
Simon Ball <simon.ball@tum.de>
2025-06-11 17:41:06 +0200

    Restructured code to make it more modular

--- lsy_drone_racing/control/attitude_mpc_collision_avoidance.py
+++ lsy_drone_racing/control/attitude_mpc_collision_avoidance.py
@@ -12,14 +12,14 @@ from __future__ import annotations  # Python 3.10 type hints
 from typing import TYPE_CHECKING
 
 import numpy as np
-import scipy
-from acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver
-from casadi import MX, cos, diag, horzcat, mtimes, sin, vertcat
+from acados_template import AcadosOcpSolver
 from scipy.interpolate import CubicSpline
 from scipy.spatial.transform import Rotation as R
 
 from lsy_drone_racing.control import Controller
-from lsy_drone_racing.utils import draw_cylinder, draw_ellipsoid, draw_line
+from lsy_drone_racing.control.acados_model import export_quadrotor_ode_model, setup_ocp
+from lsy_drone_racing.control.collision_avoidance import CollisionAvoidanceHandler
+from lsy_drone_racing.utils import draw_line
 
 if TYPE_CHECKING:
     from numpy.typing import NDArray
@@ -28,95 +28,8 @@ if TYPE_CHECKING:
 
 OBSTACLE_RADIUS = 0.15  # Radius of the obstacles in meters
 GATE_LENGTH = 0.50  # Length of the gate in meters
-ELLIPSOID_DIAMETER = 0.3  # Diameter of the ellipsoid in meters
+ELLIPSOID_RADIUS = 0.15  # Diameter of the ellipsoid in meters
 ELLIPSOID_LENGTH = 0.7  # Length of the ellipsoid in meters
-
-
-def export_quadrotor_ode_model() -> AcadosModel:
-    """Symbolic Quadrotor Model."""
-    # Define name of solver to be used in script
-    model_name = "lsy_example_mpc"
-
-    # Define Gravitational Acceleration
-    GRAVITY = 9.806
-
-    # Sys ID Params
-    params_pitch_rate = [-6.003842038081178, 6.213752925707588]
-    params_roll_rate = [-3.960889336015948, 4.078293254657104]
-    params_yaw_rate = [-0.005347588299390372, 0.0]
-    params_acc = [20.907574256269616, 3.653687545690674]
-
-    """Model setting"""
-    # define basic variables in state and input vector
-    px = MX.sym("px")  # 0
-    py = MX.sym("py")  # 1
-    pz = MX.sym("pz")  # 2
-    vx = MX.sym("vx")  # 3
-    vy = MX.sym("vy")  # 4
-    vz = MX.sym("vz")  # 5
-    roll = MX.sym("r")  # 6
-    pitch = MX.sym("p")  # 7
-    yaw = MX.sym("y")  # 8
-    f_collective = MX.sym("f_collective")
-
-    f_collective_cmd = MX.sym("f_collective_cmd")
-    r_cmd = MX.sym("r_cmd")
-    p_cmd = MX.sym("p_cmd")
-    y_cmd = MX.sym("y_cmd")
-
-    df_cmd = MX.sym("df_cmd")
-    dr_cmd = MX.sym("dr_cmd")
-    dp_cmd = MX.sym("dp_cmd")
-    dy_cmd = MX.sym("dy_cmd")
-
-    # define state and input vector
-    states = vertcat(
-        px,
-        py,
-        pz,
-        vx,
-        vy,
-        vz,
-        roll,
-        pitch,
-        yaw,
-        f_collective,
-        f_collective_cmd,
-        r_cmd,
-        p_cmd,
-        y_cmd,
-    )
-    inputs = vertcat(df_cmd, dr_cmd, dp_cmd, dy_cmd)
-
-    # Define nonlinear system dynamics
-    f = vertcat(
-        vx,
-        vy,
-        vz,
-        (params_acc[0] * f_collective + params_acc[1])
-        * (cos(roll) * sin(pitch) * cos(yaw) + sin(roll) * sin(yaw)),
-        (params_acc[0] * f_collective + params_acc[1])
-        * (cos(roll) * sin(pitch) * sin(yaw) - sin(roll) * cos(yaw)),
-        (params_acc[0] * f_collective + params_acc[1]) * cos(roll) * cos(pitch) - GRAVITY,
-        params_roll_rate[0] * roll + params_roll_rate[1] * r_cmd,
-        params_pitch_rate[0] * pitch + params_pitch_rate[1] * p_cmd,
-        params_yaw_rate[0] * yaw + params_yaw_rate[1] * y_cmd,
-        10.0 * (f_collective_cmd - f_collective),
-        df_cmd,
-        dr_cmd,
-        dp_cmd,
-        dy_cmd,
-    )
-
-    # Initialize the nonlinear model for NMPC formulation
-    model = AcadosModel()
-    model.name = model_name
-    model.f_expl_expr = f
-    model.f_impl_expr = None
-    model.x = states
-    model.u = inputs
-
-    return model
 
 
 class MPController(Controller):
@@ -134,6 +47,7 @@ class MPController(Controller):
         super().__init__(obs, info, config)
         self.freq = config.env.freq
         self._tick = 0
+        self.obs = obs
 
         # Same waypoints as in the trajectory controller. Determined by trial and error.
         waypoints = np.array(
@@ -153,7 +67,8 @@ class MPController(Controller):
                 obs["gates_pos"][2] + [0.2, -0.5, 0],
                 obs["gates_pos"][2] + [0.1, 0, 0],
                 obs["gates_pos"][2] + [0.1, 0.15, 0],
-                obs["gates_pos"][2] + [0.1, 0.15, 1],
+                obs["gates_pos"][2] + [0.1, 0.15, 0.4],
+                obs["gates_pos"][2] + [0.1, 0.15, 0.8],
                 obs["obstacles_pos"][3] + [0.4, 0.3, -0.2],
                 obs["obstacles_pos"][3] + [0.4, 0, -0.2],
                 obs["gates_pos"][3],
@@ -180,232 +95,32 @@ class MPController(Controller):
         self.y_des = np.concatenate((self.y_des, [self.y_des[-1]] * (2 * self.N + 1)))
         self.z_des = np.concatenate((self.z_des, [self.z_des[-1]] * (2 * self.N + 1)))
 
-        self.obs = obs
-        self._create_ocp_solver()
+        # Setup collision avoidance
+        num_gates = len(obs["gates_pos"])
+        num_obstacles = len(obs["obstacles_pos"])
+        self.collision_avoidance_handler = CollisionAvoidanceHandler(
+            num_gates,
+            num_obstacles,
+            GATE_LENGTH,
+            ELLIPSOID_LENGTH,
+            ELLIPSOID_RADIUS,
+            OBSTACLE_RADIUS,
+        )
+
+        # Setup the acados model and solver
+        model = export_quadrotor_ode_model()
+        self.collision_avoidance_handler.setup_model(model)
+        ocp = setup_ocp(model, self.T_HORIZON, self.N)
+        self.collision_avoidance_handler.setup_ocp(ocp)
+        self.acados_ocp_solver = AcadosOcpSolver(
+            ocp, json_file="lsy_example_mpc.json", verbose=True
+        )
 
         self.last_f_collective = 0.3
         self.last_rpy_cmd = np.zeros(3)
         self.last_f_cmd = 0.3
         self.config = config
         self.finished = False
-
-    def _create_ocp_solver(self) -> None:
-        """Create the OCP solver for the MPC."""
-        # Create the OCP solver with the predefined model and settings
-        self.ocp = AcadosOcp()
-
-        # set model
-        self.ocp.model = export_quadrotor_ode_model()
-
-        # Get Dimensions
-        nx = self.ocp.model.x.rows()
-        nu = self.ocp.model.u.rows()
-        ny = nx + nu
-        ny_e = nx
-
-        # Set horizon
-        self.ocp.solver_options.N_horizon = self.N
-
-        ## Set Cost
-        # For more Information regarding Cost Function Definition in Acados: https://github.com/acados/acados/blob/main/docs/problem_formulation/problem_formulation_ocp_mex.pdf
-
-        # Cost Type
-        self.ocp.cost.cost_type = "LINEAR_LS"
-        self.ocp.cost.cost_type_e = "LINEAR_LS"
-
-        # Weights
-        Q = np.diag(
-            [
-                3.0,
-                3.0,
-                3.0,  # Position
-                0.01,
-                0.01,
-                0.01,  # Velocity
-                0.1,
-                0.1,
-                0.1,  # rpy
-                0.01,
-                0.01,  # f_collective, f_collective_cmd
-                0.01,
-                0.01,
-                0.01,
-            ]
-        )
-
-        R = np.diag([0.01, 0.01, 0.01, 0.01])
-
-        Q_e = Q.copy()
-
-        self.ocp.cost.W = scipy.linalg.block_diag(Q, R)
-        self.ocp.cost.W_e = Q_e
-
-        Vx = np.zeros((ny, nx))
-        Vx[:nx, :] = np.eye(nx)  # Only select position states
-        self.ocp.cost.Vx = Vx
-
-        Vu = np.zeros((ny, nu))
-        Vu[nx : nx + nu, :] = np.eye(nu)  # Select all actions
-        self.ocp.cost.Vu = Vu
-
-        Vx_e = np.zeros((ny_e, nx))
-        Vx_e[:nx, :nx] = np.eye(nx)  # Only select position states
-        self.ocp.cost.Vx_e = Vx_e
-
-        # Set initial references (we will overwrite these later on to make the controller track the traj.)
-        self.ocp.cost.yref = np.array(
-            [
-                1.0,
-                1.0,
-                0.4,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.35,
-                0.35,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-            ]
-        )
-
-        self.ocp.cost.yref_e = np.array(
-            [1.0, 1.0, 0.4, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.35, 0.35, 0.0, 0.0, 0.0]
-        )
-
-        # Set State Constraints
-        self.ocp.constraints.lbx = np.array([0.1, 0.1, -1.57, -1.57, -1.57])
-        self.ocp.constraints.ubx = np.array([0.55, 0.55, 1.57, 1.57, 1.57])
-        self.ocp.constraints.idxbx = np.array([9, 10, 11, 12, 13])
-
-        # Set Input Constraints
-        # ocp.constraints.lbu = np.array([-10.0, -10.0, -10.0. -10.0])
-        # ocp.constraints.ubu = np.array([10.0, 10.0, 10.0, 10.0])
-        # ocp.constraints.idxbu = np.array([0, 1, 2, 3])
-
-        # We have to set x0 even though we will overwrite it later on.
-        self.ocp.constraints.x0 = np.zeros((nx))
-
-        self.ocp.solver_options.qp_solver = "PARTIAL_CONDENSING_HPIPM"  # FULL_CONDENSING_QPOASES
-        self.ocp.solver_options.hessian_approx = "GAUSS_NEWTON"
-        self.ocp.solver_options.integrator_type = "ERK"
-        self.ocp.solver_options.nlp_solver_type = "SQP_RTI"  # SQP_RTI
-        self.ocp.solver_options.tol = 1e-5
-        self.ocp.solver_options.qp_solver_cond_N = self.N
-        self.ocp.solver_options.qp_solver_warm_start = 1
-        self.ocp.solver_options.qp_solver_iter_max = 10
-        self.ocp.solver_options.nlp_solver_max_iter = 50
-        self.ocp.solver_options.tf = self.T_HORIZON
-
-        self._create_collisions(self.obs)
-
-        self.acados_ocp_solver = AcadosOcpSolver(
-            self.ocp, json_file="lsy_example_mpc.json", verbose=True
-        )
-
-    def _create_collisions(self, obs: dict[str, NDArray[np.floating]]) -> None:
-        """Model the collisions in the environment as nonlinear constraints in the MPC."""
-        drone_pos = self.ocp.model.x[:3]  # Get the drone position from the model state
-
-        obs_params, obs_h_expr = self._create_obstacles(drone_pos, len(obs["obstacles_pos"]))
-        gate_params, gate_h_expr = self._create_gate_collisions(drone_pos, len(obs["gates_pos"]))
-
-        self.ocp.model.p = vertcat(*obs_params, *gate_params)  # Add obstacle position as parameter
-        self.ocp.model.con_h_expr = vertcat(
-            *obs_h_expr, *gate_h_expr
-        )  # Add the nonlinear constraints
-
-        # Set nonlinear constraints
-        num_constraints = self.ocp.model.con_h_expr.rows()
-        self.ocp.dims.nsh = num_constraints
-        self.ocp.constraints.lh = np.array([0] * num_constraints)
-        self.ocp.constraints.uh = np.array([1e10] * num_constraints)
-        self.ocp.constraints.idxsh = np.array(list(range(num_constraints)))
-        self.ocp.constraints.lsh = np.array([0] * num_constraints)
-        self.ocp.constraints.ush = np.array([1e10] * num_constraints)
-        self.ocp.cost.Zl = np.array([100] * num_constraints)  # penalty on slack (lower side)
-        self.ocp.cost.Zu = np.array([0] * num_constraints)  # penalty on slack (upper side)
-        self.ocp.cost.zl = np.array([100] * num_constraints)  # desired slack is zero
-        self.ocp.cost.zu = np.array([0] * num_constraints)
-        self.ocp.parameter_values = np.array([0.0] * self.ocp.model.p.rows())
-
-    def _create_gate_collisions(self, drone_pos: MX, num_gates: int) -> tuple[MX, MX]:
-        params = []
-        h_expr = []
-
-        ellipsoid_midpoints = np.array(
-            [
-                [GATE_LENGTH / 2, 0, 0],
-                [-GATE_LENGTH / 2, 0, 0],
-                [0, 0, GATE_LENGTH / 2],
-                [0, 0, -GATE_LENGTH / 2],
-            ]
-        )
-        ellipsoid_axes = np.array(
-            [
-                [ELLIPSOID_DIAMETER / 2, ELLIPSOID_DIAMETER / 2, ELLIPSOID_LENGTH / 2],
-                [ELLIPSOID_LENGTH / 2, ELLIPSOID_DIAMETER / 2, ELLIPSOID_DIAMETER / 2],
-                [ELLIPSOID_DIAMETER / 2, ELLIPSOID_DIAMETER / 2, ELLIPSOID_LENGTH / 2],
-                [ELLIPSOID_LENGTH / 2, ELLIPSOID_DIAMETER / 2, ELLIPSOID_DIAMETER / 2],
-            ]
-        )
-
-        for i in range(num_gates):
-            gate_pos = MX.sym(f"p_gate{i}", 4)  # Gate position (x, y, z, yaw)
-            params.append(gate_pos)
-
-            center = gate_pos[:3]  # Center of the gate
-            yaw = gate_pos[3]  # Yaw angle of the gate
-            # Rotation matrix for yaw (around z)
-            Rz = vertcat(
-                horzcat(cos(yaw), -sin(yaw), 0), horzcat(sin(yaw), cos(yaw), 0), horzcat(0, 0, 1)
-            )
-
-            for midpoint, axes in zip(ellipsoid_midpoints, ellipsoid_axes):
-                midpoint_vec = MX(midpoint)
-                ellipsoid_center = center + mtimes(Rz, midpoint_vec)
-                a, b, c = axes
-                dpos = drone_pos - ellipsoid_center
-                ellipsoid_val = (
-                    dpos.T @ Rz.T @ diag([1 / a**2, 1 / b**2, 1 / c**2]) @ Rz @ dpos
-                ) - 1
-                h_expr.append(ellipsoid_val)
-
-        return params, h_expr
-
-    def _create_obstacles(self, drone_pos: MX, num_obstacles: int) -> tuple[list[MX], list[MX]]:
-        """Model the obstacles in the environment. as nonlinear constraints in the MPC."""
-        params = []
-        h_expr = []
-        for i in range(num_obstacles):
-            center_obs = MX.sym(f"p_obs{i}", 2)
-            params.append(center_obs)
-
-            # Infinitly high cylinder around the obstacle with radius OBSTACLE_RADIUS
-            pos_xy = drone_pos[:2]  # Get the x and y position of the drone
-            con_h_expr = (pos_xy - center_obs).T @ (pos_xy - center_obs) - OBSTACLE_RADIUS**2
-            h_expr.append(con_h_expr)
-        return params, h_expr
-
-    def _update_parameters(self, obs: dict[str, NDArray[np.floating]]) -> None:
-        obstacle_positions = obs["obstacles_pos"]
-        obstacle_params = obstacle_positions[:, :2].flatten()
-
-        gate_positions = obs["gates_pos"]
-        gate_rotations = R.from_quat(obs["gates_quat"]).as_euler("xyz", degrees=False)
-        gate_params = np.hstack((gate_positions, gate_rotations[:, 2:])).flatten()
-        params = np.concatenate((obstacle_params, gate_params))
-
-        for i in range(self.N):
-            gate_params = gate_positions[:, :4].flatten()
-            self.acados_ocp_solver.set(i, "p", params)
 
     def compute_control(
         self, obs: dict[str, NDArray[np.floating]], info: dict | None = None
@@ -487,7 +202,7 @@ class MPController(Controller):
         )
         self.acados_ocp_solver.set(self.N, "yref", yref_N)
 
-        self._update_parameters(obs)
+        self.collision_avoidance_handler.update_parameters(self.acados_ocp_solver, self.N, obs)
 
         self.acados_ocp_solver.solve()
         x1 = self.acados_ocp_solver.get(1, "x")
@@ -519,56 +234,7 @@ class MPController(Controller):
             max_size=0.01,
         )
 
-        obstacle_positions = self.obs["obstacles_pos"]
-        obstacle_positions[:, 2] = 0  # Only use x and y positions
-        for obstacle_pos in obstacle_positions:
-            draw_cylinder(
-                env,
-                pos=obstacle_pos,
-                size=np.array([OBSTACLE_RADIUS, 2.0]),
-                rgba=np.array([0.2, 0.2, 0.8, 0.5]),
-            )
-
-        gate_positions = self.obs["gates_pos"]
-        gate_quats = self.obs["gates_quat"]
-
-        from scipy.spatial.transform import Rotation as R
-
-        num_gates = gate_positions.shape[0]
-        ellipsoid_midpoints = np.array(
-            [
-                [GATE_LENGTH / 2, 0, 0],
-                [0, 0, GATE_LENGTH / 2],
-                [-GATE_LENGTH / 2, 0, 0],
-                [0, 0, -GATE_LENGTH / 2],
-            ]
-        )
-        ellipsoid_axes = np.array(
-            [
-                [ELLIPSOID_DIAMETER / 2, ELLIPSOID_DIAMETER / 2, ELLIPSOID_LENGTH / 2],
-                [ELLIPSOID_LENGTH / 2, ELLIPSOID_DIAMETER / 2, ELLIPSOID_DIAMETER / 2],
-                [ELLIPSOID_DIAMETER / 2, ELLIPSOID_DIAMETER / 2, ELLIPSOID_LENGTH / 2],
-                [ELLIPSOID_LENGTH / 2, ELLIPSOID_DIAMETER / 2, ELLIPSOID_DIAMETER / 2],
-            ]
-        )
-
-        gate_yaws = R.from_quat(gate_quats).as_euler("xyz")[:, 2]
-        for gate_idx in range(num_gates):
-            gate_pos = gate_positions[gate_idx]
-            yaw = gate_yaws[gate_idx]
-            # Rotation matrix for yaw (around z)
-            c = np.cos(yaw)
-            s = np.sin(yaw)
-            Rz = np.array([[c, -s, 0], [s, c, 0], [0, 0, 1]])
-            for midpoint, axes in zip(ellipsoid_midpoints, ellipsoid_axes):
-                ellipsoid_center = gate_pos + Rz @ midpoint
-                draw_ellipsoid(
-                    env,
-                    pos=ellipsoid_center,
-                    size=axes,
-                    rot=Rz,
-                    rgba=np.array([0.8, 0.2, 0.2, 0.5]),
-                )
+        self.collision_avoidance_handler.draw_collision_bodies(env, self.obs)
 
     def step_callback(
         self,
---
+++ lsy_drone_racing/control/collision_avoidance.py
@@ -0,0 +1,229 @@
+"""This module handles the collision avoidance by setting up the constraints for the gates and obstacles in the environment."""
+
+import numpy as np
+from acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver
+from casadi import MX, cos, diag, horzcat, sin, vertcat
+from numpy.typing import NDArray
+from scipy.spatial.transform import Rotation
+
+from lsy_drone_racing.envs.drone_race import DroneRaceEnv
+from lsy_drone_racing.utils import draw_cylinder, draw_ellipsoid
+
+
+class CollisionAvoidanceHandler:
+    """Handles the collision avoidance by setting up the constraints for the gates and obstacles in the environment.
+
+    This class defines ellipsoids representing the gates and infinite cylinders for the obstacles.
+    It sets them as constraints in the model / OCP and provides methods to update parameters and draw collision bodies in the environment.
+    """
+
+    def __init__(
+        self,
+        num_gates: int,
+        num_obstacles: int,
+        gate_length: float,
+        ellipsoid_length: float,
+        ellipsoid_radius: float,
+        obstacle_radius: float,
+    ):
+        """Constructor for the CollisionAvoidanceHandler.
+
+        Args:
+            num_gates (int): Number of gates in the environment.
+            num_obstacles (int): Number of obstacles in the environment.
+            gate_length (float): Length of the gate in meters.
+            ellipsoid_length (float): Length of the ellipsoid representing the border of the gates.
+            ellipsoid_radius (float): Radius of the ellipsoid representing the border of the gates.
+            obstacle_radius (float): Radius of the infinite cylinder representing the obstacles.
+        """
+        self.num_gates = num_gates
+        self.num_obstacles = num_obstacles
+        self.gate_length = gate_length
+        self.ellipsoid_length = ellipsoid_length
+        self.ellipsoid_radius = ellipsoid_radius
+        self.obstacle_radius = obstacle_radius
+
+        self.num_constraints = num_gates * 4 + num_obstacles
+
+        # Midpoints of the four ellipsoids representing the gate
+        self.ellipsoid_midpoints = np.array(
+            [
+                [self.gate_length / 2, 0, 0],
+                [0, 0, self.gate_length / 2],
+                [-self.gate_length / 2, 0, 0],
+                [0, 0, -self.gate_length / 2],
+            ]
+        )
+
+        # Axes of the four ellipsoids representing the gate
+        self.ellipsoid_axes = np.array(
+            [
+                [self.ellipsoid_radius, self.ellipsoid_radius, self.ellipsoid_length / 2],
+                [self.ellipsoid_length / 2, self.ellipsoid_radius, self.ellipsoid_radius],
+                [self.ellipsoid_radius, self.ellipsoid_radius, self.ellipsoid_length / 2],
+                [self.ellipsoid_length / 2, self.ellipsoid_radius, self.ellipsoid_radius],
+            ]
+        )
+
+    def setup_model(self, model: AcadosModel):
+        """Set up the model with the collision avoidance constraints.
+
+        Args:
+            model (AcadosModel): The acados model to which the collision avoidance constraints will be added.
+        """
+        pos = model.x[:3]
+
+        obs_params, obs_h_expr = self._create_obstacle_expressions(pos)
+        gate_params, gate_h_expr = self._create_gate_expressions(pos)
+
+        model.p = vertcat(*obs_params, *gate_params)
+        model.con_h_expr = vertcat(*obs_h_expr, *gate_h_expr)
+
+    def setup_ocp(self, ocp: AcadosOcp):
+        """Set up the OCP with the collision avoidance constraints.
+
+        Args:
+            ocp (AcadosOCP): The acados OCP to which the collision avoidance constraints will be added.
+        """
+        ocp.dims.nsh = self.num_constraints
+
+        # Set lower and upper bounds for the constraints
+        ocp.constraints.lh = np.array([0] * self.num_constraints)
+        ocp.constraints.uh = np.array([1e10] * self.num_constraints)
+
+        # Slack variables for the constraints
+        ocp.constraints.idxsh = np.arange(self.num_constraints)
+        ocp.constraints.lsh = np.array([0] * self.num_constraints)
+        ocp.constraints.ush = np.array([1e10] * self.num_constraints)
+
+        # Set the cost for constraint violations
+        ocp.cost.Zl = np.array([100] * self.num_constraints)
+        ocp.cost.Zu = np.array([0] * self.num_constraints)
+        ocp.cost.zl = np.array([100] * self.num_constraints)
+        ocp.cost.zu = np.array([0] * self.num_constraints)
+
+        # Set initial parameter values
+        ocp.parameter_values = np.array([0.0] * ocp.model.p.rows())
+
+    def update_parameters(
+        self, ocp_solver: AcadosOcpSolver, N_horizon: int, obs: dict[str, NDArray[np.floating]]
+    ):
+        """Update the parameters of the OCP solver with the current obstacle and gate positions.
+
+        Args:
+            ocp_solver (AcadosOcpSolver): The acados OCP solver to update the parameters for.
+            N_horizon (int): Number of time steps in the horizon.
+            obs (dict[str, NDArray[np.floating]]): The current observation of the environment containing obstacle and gate positions.
+        """
+        obstacle_positions = obs["obstacles_pos"]
+        obstacle_params = obstacle_positions[:, :2].flatten()
+
+        gate_positions = obs["gates_pos"]
+        gate_rotations = Rotation.from_quat(obs["gates_quat"]).as_euler("xyz", degrees=False)
+        gate_params = np.hstack((gate_positions, gate_rotations[:, 2:])).flatten()
+        params = np.concatenate((obstacle_params, gate_params))
+
+        for i in range(N_horizon):
+            gate_params = gate_positions[:, :4].flatten()
+            ocp_solver.set(i, "p", params)
+
+    def draw_collision_bodies(self, env: DroneRaceEnv, obs: dict[str, NDArray[np.floating]]):
+        """Draw the collision bodies (gates and obstacles) in the environment.
+
+        Args:
+            env (DroneRaceEnv): The drone racing environment where the collision bodies will be drawn.
+            obs (dict[str, NDArray[np.floating]]): The current observation of the environment containing obstacle and gate positions.
+        """
+        # Draw obstacles
+        obstacle_positions = obs["obstacles_pos"]
+        obstacle_positions[:, 2] = 0  # Only use x and y values
+        for obstacle_pos in obstacle_positions:
+            draw_cylinder(
+                env,
+                pos=obstacle_pos,
+                size=np.array([self.obstacle_radius, 2.0]),
+                rgba=np.array([0.2, 0.2, 0.8, 0.5]),
+            )
+
+        # Draw gates
+        gate_positions = obs["gates_pos"]
+        gate_quats = obs["gates_quat"]
+        gate_yaws = Rotation.from_quat(gate_quats).as_euler("xyz")[:, 2]  # Extract yaw angles
+        for pos, yaw in zip(gate_positions, gate_yaws):
+            # Rotation matrix for yaw (around z)
+            c = np.cos(yaw)
+            s = np.sin(yaw)
+            Rz = np.array([[c, -s, 0], [s, c, 0], [0, 0, 1]])
+
+            for midpoint, axes in zip(self.ellipsoid_midpoints, self.ellipsoid_axes):
+                ellipsoid_center = pos + Rz @ midpoint
+                draw_ellipsoid(
+                    env,
+                    pos=ellipsoid_center,
+                    size=axes,
+                    rot=Rz,
+                    rgba=np.array([0.8, 0.2, 0.2, 0.5]),
+                )
+
+    def _create_gate_expressions(self, drone_pos: MX) -> tuple[list[MX], list[MX]]:
+        """Create parameters and expressions for ellipsoidal collision bodies for the gates.
+
+        Args:
+            drone_pos (MX): The position of the drone in the form of a CasADi MX variable.
+
+        Returns:
+            tuple[list[MX], list[MX]]: A tuple containing two lists:
+                - params: List of MX variables representing the gate positions.
+                - h_expr: List of MX expressions representing the ellipsoidal constraints for the gates.
+        """
+        params = []
+        h_expr = []
+        for i in range(self.num_gates):
+            gate_pos = MX.sym(f"p_gate{i}", 4)  # Gate position (x, y, z, yaw)
+            params.append(gate_pos)
+
+            center = gate_pos[:3]  # Center of the gate
+            yaw = gate_pos[3]  # Yaw angle of the gate
+
+            # Rotation matrix for yaw (around z)
+            Rz = vertcat(
+                horzcat(cos(yaw), -sin(yaw), 0), horzcat(sin(yaw), cos(yaw), 0), horzcat(0, 0, 1)
+            )
+
+            for midpoint, axes in zip(self.ellipsoid_midpoints, self.ellipsoid_axes):
+                ellipsoid_center = center + Rz @ MX(midpoint)
+                a, b, c = axes  # Axes of the ellipsoid
+
+                dpos = drone_pos - ellipsoid_center
+
+                # Ellipsoid equation
+                ellipsoid_exp = (
+                    dpos.T @ Rz.T @ diag([1 / a**2, 1 / b**2, 1 / c**2]) @ Rz @ dpos
+                ) - 1
+                h_expr.append(ellipsoid_exp)
+
+        return params, h_expr
+
+    def _create_obstacle_expressions(self, drone_pos: MX) -> tuple[list[MX], list[MX]]:
+        """Create parameters and expressions for infinite cylinder collision bodies for the obstacles.
+
+        Args:
+            drone_pos (MX): The position of the drone in the form of a CasADi MX variable.
+
+        Returns:
+            tuple[list[MX], list[MX]]: A tuple containing two lists:
+                - params: List of MX variables representing the obstacle positions.
+                - h_expr: List of MX expressions representing the cylindrical constraints for the obstacles.
+        """
+        params = []
+        h_expr = []
+        for i in range(self.num_obstacles):
+            center_obs = MX.sym(f"p_obs{i}", 2)
+            params.append(center_obs)
+
+            # Infinitly high cylinder around the obstacle with radius OBSTACLE_RADIUS
+            pos_xy = drone_pos[:2]  # Get the x and y position of the drone
+            con_h_expr = (pos_xy - center_obs).T @ (pos_xy - center_obs) - self.obstacle_radius**2
+            h_expr.append(con_h_expr)
+
+        return params, h_expr
