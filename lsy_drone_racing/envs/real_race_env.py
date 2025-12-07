Refactored rotation check: now rotations are checked in three axes. Fixed minor issue: ROSConnector timeout values are increased to 10 seconds. Fixed minor issue: The update_level_config function missed a key in dict query, now it works. Added take off flag: A flag will stay True in RealRaceCoreEnv after taking off(once z coordinate > 0.2 meter), and will be queried when calling env.close(). If the drone has not "taken off", the return back controller will not be called. Tested cases: See https://docs.google.com/spreadsheets/d/1a_WvakzvkbI9FE5ZeVwlMI_acAK3nL8hTaW2EinpsPs/edit?usp=sharing

lsy_drone_racing/envs/real_race_env.py
@@ -127,7 +127,7 @@ class RealRaceCoreEnv:
         )
         # A hatched indicator whether the drone has taken off at least once
         # Used to prevent the return controller from engaging before takeoff
-        self.taken_off = False
+        self._taken_off = False
         # Dynamic data
         self.data = EnvData.create(
             n_drones=self.n_drones, n_gates=self.n_gates, n_obstacles=self.n_obstacles
@@ -175,7 +175,7 @@ class RealRaceCoreEnv:
         self.data.target_gate += np.asarray(passed)
         self.data.target_gate[self.data.target_gate >= self.n_gates] = -1
         self.data.last_drone_pos[...] = drone_pos
-        self.taken_off |= (drone_pos[self.rank, 2] > 0.2)
+        self._taken_off |= drone_pos[self.rank, 2] > 0.2
         # Send vicon position updates to the drone at a fixed frequency irrespective of the env freq
         # Sending too many updates may deteriorate the performance of the drone, hence the limiter
         if (t := time.perf_counter()) - self._last_drone_pos_update > 1 / self.POS_UPDATE_FREQ:
@@ -188,11 +188,13 @@ class RealRaceCoreEnv:
         # If gates/obstacles are in sensor range use the actual pose, otherwise use the nominal pose
         # The actual pose is measured at the beginning of the episode and is not updated during the
         # episode. If we want to use dynamic gates/obstacles, we need to update the poses here.
-        mask = self.data.gates_visited[..., None]
-        gates_pos = np.where(mask, self.gates.pos, self.gates.nominal_pos).astype(np.float32)
-        gates_quat = np.where(mask, self.gates.quat, self.gates.nominal_quat).astype(np.float32)
-        mask = self.data.obstacles_visited[..., None]
-        obstacles_pos = np.where(mask, self.obstacles.pos, self.obstacles.nominal_pos).astype(
+        mask_gate = self.data.gates_visited[..., None]
+        gates_pos = np.where(mask_gate, self.gates.pos, self.gates.nominal_pos).astype(np.float32)
+        gates_quat = np.where(mask_gate, self.gates.quat, self.gates.nominal_quat).astype(
+            np.float32
+        )
+        mask_obs = self.data.obstacles_visited[..., None]
+        obstacles_pos = np.where(mask_obs, self.obstacles.pos, self.obstacles.nominal_pos).astype(
             np.float32
         )
         drone_pos = np.stack([self._ros_connector.pos[drone] for drone in self.drone_names])
@@ -275,7 +277,7 @@ class RealRaceCoreEnv:
             tf_names = [f"gate{i}" for i in range(1, self.n_gates + 1)]
             tf_names += [f"obstacle{i}" for i in range(1, self.n_obstacles + 1)]
             try:
-                ros_connector = ROSConnector(tf_names=tf_names, timeout=5.0)
+                ros_connector = ROSConnector(tf_names=tf_names, timeout=10.0)
                 pos, quat = ros_connector.pos, ros_connector.quat
             finally:
                 ros_connector.close()
@@ -293,6 +295,11 @@ class RealRaceCoreEnv:
                 )
                 raise
         if track.randomize:
+            if not deploy_config.get("real_track_objects", True):
+                raise ValueError(
+                    "With randomizely generated track (env.track.randomize=True), "
+                    "the deploy.real_track_objects should always be set to True."
+                )
             self.gates.nominal_pos, self.gates.nominal_quat, self.obstacles.nominal_pos = (
                 randomize_track(
                     gates_pos=self.gates.pos,
@@ -454,8 +461,6 @@ class RealRaceCoreEnv:
         pos = self._ros_connector.pos[self.drone_name]
         vel = self._ros_connector.vel[self.drone_name]
         # This quick check prevents us from engaging the return controller if we havent even started yet.
-        if not self.taken_off:
-            return
         break_pos = pos + vel / np.linalg.norm(vel) * BREAKING_DISTANCE
         break_pos[2] = RETURN_HEIGHT
         self.drone.high_level_commander.go_to(*break_pos, 0, BREAKING_DURATION)
@@ -496,7 +501,7 @@ class RealRaceCoreEnv:
         # Now the track.randomize needs be set to false,
         # since we are storing the layout from a real-world placement
         result_config["env"]["track"]["randomize"] = False
-        
+
         # Overwrite the original field with the actual positions from the env
         result_config["env"]["track"]["gates"] = [
             {
@@ -508,7 +513,7 @@ class RealRaceCoreEnv:
         result_config["env"]["track"]["obstacles"] = [
             {"pos": (self.obstacles.pos[i]).tolist()} for i in range(self.obstacles.pos.shape[0])
         ]
-        result_config["track"]["drones"] = [
+        result_config["env"]["track"]["drones"] = [
             {
                 "pos": (self.drones.pos[i]).tolist(),
                 "rpy": (R.from_quat(self.drones.quat[i]).as_euler("xyz", degrees=False)).tolist(),
@@ -527,7 +532,7 @@ class RealRaceCoreEnv:
         case of errors, and close the connections to the ROSConnector.
         """
         try:
-            if self._connected_to_drone:
+            if self._connected_to_drone and self._taken_off:
                 self._return_to_start()
         finally:
             try:

lsy_drone_racing/utils/checks.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Union
 
 import numpy as np
 from scipy.spatial.transform import Rotation as R
@@ -62,11 +62,11 @@ def check_gates_in_bound(gates: ConfigDict, limits: ConfigDict):
     for i, pos in enumerate(gates.pos):
         if np.any(pos[:2] < limits.pos_limit_low):
             raise RuntimeError(
-                f"gate{i + 1} position {pos[:2]} is below the minimum limit {limits.pos_limit_low}"
+                f"gate{i + 1} position {pos[:2]} is below the predefined minimum limit {limits.pos_limit_low}"
             )
         if np.any(pos[:2] > limits.pos_limit_high):
             raise RuntimeError(
-                f"gate{i + 1} position {pos[:2]} is above the maximum limit {limits.pos_limit_high}"
+                f"gate{i + 1} position {pos[:2]} is above the predefined maximum limit {limits.pos_limit_high}"
             )
 
 
@@ -86,11 +86,13 @@ def check_race_track(gates: ConfigDict, obstacles: ConfigDict, rng_config: Confi
         check_bounds(f"gate{i + 1}", pos, nominal_pos, np.array(low), np.array(high))
 
     # TODO: Now the gate check should consider rotation in roll and pitch as well.
-    ang_tol = rng_config.gate_rpy.kwargs.maxval[2]  # Only check yaw rotation
+    high_tol = np.array(rng_config.gate_rpy.kwargs.maxval)
+    low_tol = np.array(rng_config.gate_rpy.kwargs.minval)
+    # ang_tol = rng_config.gate_rpy.kwargs.maxval[2]  # Only check yaw rotation
     for i, (quat, nominal_quat) in enumerate(zip(gates.quat, gates.nominal_quat)):
         gate_rot = R.from_quat(quat)
         nominal_rot = R.from_quat(nominal_quat)
-        check_rotation(f"gate{i + 1}", gate_rot, nominal_rot, ang_tol)
+        check_rotation(f"gate{i + 1}", gate_rot, nominal_rot, low=low_tol, high=high_tol)
 
     low, high = rng_config.obstacle_pos.kwargs.minval, rng_config.obstacle_pos.kwargs.maxval
     for i, (pos, nominal_pos) in enumerate(zip(obstacles.pos, obstacles.nominal_pos)):
@@ -121,15 +123,16 @@ def check_drone_start_pos(
 
 
 def check_bounds(name: str, actual: NDArray, desired: NDArray, low: NDArray, high: NDArray):
-    """Check if the actual value is within the specified bounds of the desired value.\
-        Raise error when the actual value exceeds the low or high boundaries.
+    """Check if the actual value is within the specified bounds of the desired value.
+
+    Raise error when the actual value exceeds the low or high boundaries.
 
     Args:
         name: Name of the object being checked.
         actual: NDArray of real value and is expected to be [N, ].
         desired: NDArray of nominal value and is expected to be [N, ].
         low: NDArray of the minimum value of (actual - desired) and is expected to be [N, ]
-        high: NDArray of the maximum value of (actual - desired) and is expected to be [N, ]    
+        high: NDArray of the maximum value of (actual - desired) and is expected to be [N, ]
     """
     if np.any(actual - desired < low):
         raise RuntimeError(
@@ -141,11 +144,46 @@ def check_bounds(name: str, actual: NDArray, desired: NDArray, low: NDArray, hig
         )
 
 
-def check_rotation(name: str, actual_rot: R, desired_rot: R, ang_tol: float):
-    """Check if the actual rotation is within the specified tolerance of the desired rotation."""
-    if (actual_rot.inv() * desired_rot).magnitude() > ang_tol:
-        actual, desired = actual_rot.as_euler("xyz"), desired_rot.as_euler("xyz")
-        raise RuntimeError(
-            f"{name} exceeds rotation tolerances ({ang_tol}).\n"
-            f"Rotation is: {actual}, should be: {desired}"
-        )
+def check_rotation(
+    name: str,
+    actual_rot: R,
+    desired_rot: R,
+    low: Union[np.ndarray, float],
+    high: Union[np.ndarray, float],
+):
+    """Compare gate orientations in world-frame Euler XYZ if the ang_tol is an array. Otherwise, check the overall magnitude of the rotation.
+
+    Args:
+        name: Name of the object being checked.
+        actual_rot: scipy.spatial.transform.R object describing rotation of the real object.
+        desired_rot:  scipy.spatial.transform.R object describing rotation of the nominal object.
+        low: A float indicating the overall magnitude lower limit or a NDArray designating the per axis rotation lower limit
+        high: A float indicating the overall magnitude higher limit or a NDArray designating the per axis rotation higher limit
+
+    """
+    assert not np.isscalar(low) ^ np.isscalar(high), (
+        f"Lower and higher bound should be all scalar or array, got low={low} and high={high}"
+    )
+    actual = actual_rot.as_euler("xyz", degrees=False)
+    desired = desired_rot.as_euler("xyz", degrees=False)
+
+    if isinstance(low, float) and isinstance(high, float):
+        if (actual_rot.inv() * desired_rot).magnitude() > max(abs(low), abs(high)):
+            raise RuntimeError(
+                f"{name} exceeds rotation tolerances ({max(abs(low), abs(high))}).\n"
+                f"Rotation is: {actual}, should be: {desired}"
+            )
+    else:
+        # Angle difference with wrap-around
+        diff = (actual - desired + np.pi) % (2 * np.pi) - np.pi
+
+        if np.any(diff < low):
+            raise RuntimeError(
+                f"{name} exceeds lower rotation tolerances ({low}).\n"
+                f"Rotation is: {actual}, should be: {desired}"
+            )
+        elif np.any(diff > high):
+            raise RuntimeError(
+                f"{name} exceeds higher rotation tolerances ({high}).\n"
+                f"Rotation is: {actual}, should be: {desired}"
+            )